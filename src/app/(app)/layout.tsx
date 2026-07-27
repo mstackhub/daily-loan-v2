1991466ad5e77ea8f5ba69ee5ffbb60213a16477
mstackhub <m.aekungkun@gmail.com>
2026-07-27 15:57:49 +0700

    layout: remove stacking context constraint from main tag to let modals overlay BottomNav

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       </div>
 
       {/* Main content */}
-      <main className="relative z-10 pb-28">
+      <main className="pb-28">
         {children}
       </main>
 
